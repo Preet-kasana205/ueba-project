0987e956b436a8e67950bea24e6b4efa0e65ce21
Preet-kasana205git config --global user.email kasanap553@gmail.com <kasanap553@gmail.com>
2026-06-12 10:59:09 +0530

    add anomaly model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,14 +1,7 @@
-from app.models.baseline import Baseline
-from app.models.device import UserDevice
-from app.models.event import NormalizedEvent, RawEvent
 from app.models.user import User
+from app.models.event import RawEvent, NormalizedEvent
+from app.models.device import UserDevice
+from app.models.baseline import Baseline
+from app.models.anomaly import Anomaly
 
 
-__all__ = [
-    
-    "Baseline",
-    "NormalizedEvent",
-    "RawEvent",
-    "User",
-    "UserDevice",
-]
